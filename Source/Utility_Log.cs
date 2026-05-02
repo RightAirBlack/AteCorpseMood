54425ad6a62d99d11db6d829a8abd7e3d721aa09
RightAirBlack <rightji@outlook.com>
2026-05-02 17:16:45 +0800

    basically completed

--- Source/Utility_Log.cs
+++ Source/Utility_Log.cs
@@ -1,11 +1,13 @@
 ﻿
 using Verse;
-
-public static class L
+namespace AteCorpseMood
 {
-    public static readonly string PREFIX = "[Rt's AteCorpseMood] ";
-    public static void W(string txt) => Log.Warning(PREFIX + txt);
-    public static void E(string txt) => Log.Error(PREFIX + txt);
-    public static void M(string txt) => Log.Message(PREFIX + txt);
-    public static void M(object obj) => L.M(obj.ToString());
+    public static class L
+    {
+        public static readonly string PREFIX = "[Rt's AteCorpseMood] ";
+        public static void W(string txt) => Log.Warning(PREFIX + txt);
+        public static void E(string txt) => Log.Error(PREFIX + txt);
+        public static void M(string txt) => Log.Message(PREFIX + txt);
+        public static void M(object obj) => L.M(obj.ToString());
+    }
 }
